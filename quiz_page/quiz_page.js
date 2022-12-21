4b487d78ff67bcb1913905183af6ce68fc3834f9
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2022-12-21 18:36:44 +0100

    request body

--- quiz_page/quiz_page.js
+++ quiz_page/quiz_page.js
@@ -4,18 +4,11 @@ const button2 = document.getElementsByClassName('btn2');
 const button3 = document.getElementsByClassName('btn3');
 
 const array = ['PIA00122', 'PIA00126', 'GSFC_20171208_Archive_e001435']
+const id = array[0]
 
-var id = array[Math.floor(Math.random()*array.length)];
-
-const getSuggestions = async() => {
-    const endpoint = `https://images-api.nasa.gov/search?nasa_id=${id}`
-    try {
-        const response = await fetch(endpoint, {cache: 'no-cache'})
-        if (response.ok) {
-            const jsonResponse = await response.json()
-            renderResponse(jsonResponse)
-        }
-    } catch(error) {
-        console.log(error)
-    }
-}
+fetch(`https://images-api.nasa.gov/search?nasa_id=${id}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err));
