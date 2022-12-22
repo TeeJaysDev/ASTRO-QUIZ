7afb012797cd4fceb56bce0b52cd4d601f5e5ff7
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2022-12-22 18:25:01 +0100

    image added

--- quiz_page/quiz_page.js
+++ quiz_page/quiz_page.js
@@ -1,14 +1,21 @@
-const image = document.getElementsByClassName('img');
+const section = document.getElementById('section');
 const button1 = document.getElementsByClassName('btn1');
 const button2 = document.getElementsByClassName('btn2');
 const button3 = document.getElementsByClassName('btn3');
 
 const array = ['PIA00122', 'PIA00126', 'GSFC_20171208_Archive_e001435']
-const id = array[0]
+let id = []
+
+array.forEach(element => {
+    id = element
+})
 
 fetch(`https://images-api.nasa.gov/search?nasa_id=${id}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => console.log(err));
+.then(response => response.json())
+.then(data => {
+    let img=document.createElement('img');
+    img.src = data.collection.items[0].links[0].href;
+    section.appendChild(img);
+    console.log(data);
+})
+.catch(err => console.log(err));
