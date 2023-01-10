c8a64dd4fc4d1c5d79f131481a533d9065965e78
Tymon 'TeeJay' Jędryczka <jedryczkatymon@gmail.com>
2023-01-10 20:50:23 +0100

    random questions

--- quiz_page/quiz_page.js
+++ quiz_page/quiz_page.js
@@ -1,19 +1,46 @@
+
 const section = document.getElementById('section');
-const button1 = document.getElementsByClassName('btn1');
-const button2 = document.getElementsByClassName('btn2');
-const button3 = document.getElementsByClassName('btn3');
+const button1 = document.getElementsByClassName('btn')[0];
+const button2 = document.getElementsByClassName('btn')[1];
+const button3 = document.getElementsByClassName('btn')[2];
 
-const array = ['PIA00122', 'PIA00126', 'GSFC_20171208_Archive_e001435']
-let id = []
+const questions = [
+    {
+        image: 'https://images-api.nasa.gov/search?nasa_id=PIA00122',
+        answers: [
+            { text: 'Earth', correct: true },
+            { text: 'Sun', correct: false },
+            { text: 'Moon', correct: false },
+        ]
+    },
+    {
+        image: 'https://images-api.nasa.gov/search?nasa_id=PIA00126',
+        answers: [
+            { text: 'Mars', correct: false },
+            { text: 'Moon', correct: true },
+            { text: 'Black Hole', correct: false },
+        ]
+    },
+    {
+        image: 'https://images-api.nasa.gov/search?nasa_id=GSFC_20171208_Archive_e001435',
+        answers: [
+            { text: 'Neutron Star', correct: false },
+            { text: 'Sun', correct: true },
+            { text: 'Garghantua', correct: false },
+        ]
+    }
+]
 
-array.forEach(element => {
-    id = element
-})
+const max = questions.length;
 
-fetch(`https://images-api.nasa.gov/search?nasa_id=${id}`)
+const index = (integer) => {
+    return Math.floor(Math.random() * integer);
+};
+
+fetch(questions[index(max)].image)
 .then(response => response.json())
 .then(data => {
-    let img=document.createElement('img');
+    const img = document.createElement('img');
     img.src = data.collection.items[0].links[0].href;
     section.appendChild(img);
     console.log(data);
